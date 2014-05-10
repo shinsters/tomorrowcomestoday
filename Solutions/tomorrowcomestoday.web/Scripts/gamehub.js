d9492543c59a88e4478e0b023acdcf41b1a3f0d6
John Harrison <john@johnharrison.me>
2014-05-10 21:28:13 +0100

    - Populated variables from initial state

--- Solutions/tomorrowcomestoday.web/Scripts/gamehub.js
+++ Solutions/tomorrowcomestoday.web/Scripts/gamehub.js
@@ -16,12 +16,33 @@ app.controller('GameController', function ($scope) {
     // Reference the auto-generated proxy for the hub.  
     var gameHub = $.connection.gameHub;
 
+    $scope.cardsInHand = [];
+    $scope.playerNames = [];
+    $scope.blackCardText = "";
+    $scope.players = [];
+    $scope.playerGameGuid = "";
+
     gameHub.client.broadcastMessage = function (message) {
     }
 
+    /// Called when user has been joined to a game, contains state of game
     gameHub.client.sendInitialState = function (gameInitialStateViewModel) {
-        var viewModelAsString = JSON.stringify(gameInitialStateViewModel);
-        alert(viewModelAsString);
+
+        // grab out the cards in hand
+        angular.forEach(gameInitialStateViewModel.DealtCards, function (item) {
+            $scope.cardsInHand.push(item);
+        });
+
+        // grab out the players
+        angular.forEach(gameInitialStateViewModel.PlayerNames, function (item) {
+            $scope.playerNames.push(item);
+        });
+
+        $scope.blackCardText = gameInitialStateViewModel.BlackCardText;
+        $scope.playerGameGuid = gameInitialStateViewModel.PlayerInGameGuid;
+
+        alert("Black card: " + $scope.blackCardText);
+        alert("Player Guid: " + $scope.playerGameGuid);
     }
 
     $.connection.hub.start().done(function () {
