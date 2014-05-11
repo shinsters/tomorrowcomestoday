0fe8d7b44cfb013a3f27fbcc945fc6aa07e171e6
John Harrison <john@johnharrison.me>
2014-05-11 15:02:48 +0100

    - Got angular modules loading (I'm an idiot)

--- Solutions/tomorrowcomestoday.web/Scripts/tomorrowcomestoday/app/app.js
+++ Solutions/tomorrowcomestoday.web/Scripts/tomorrowcomestoday/app/app.js
@@ -1,5 +1,7 @@
-﻿// angular app
-var app = angular.module('app', ['ngRoute']).config(function ($routeProvider) {
+﻿var duScroll = angular.module('duScroll', []);  
+
+// angular app
+var app = angular.module('app', ['ngRoute', 'duScroll']).config(function ($routeProvider) {
 
     $routeProvider.when('/game', {
         templateUrl : '/Content/templates/game/game.html',
@@ -9,58 +11,6 @@ var app = angular.module('app', ['ngRoute']).config(function ($routeProvider) {
     $routeProvider.otherwise({
         redirectTo: '/game'
     });
-});
-
-
-// define controllers
-app.controller('GameController', function ($scope) {
-    // Reference the auto-generated proxy for the hub.  
-    var gameHub = $.connection.gameHub;
-
-    $scope.cardsInHand = [];
-    $scope.cardsInTopRow = [];
-    $scope.cardsInBottomRow = [];
-
-    $scope.playerNames = [];
-    $scope.blackCardText = "";
-    $scope.players = [];
-    $scope.playerGameGuid = "";
-
-    gameHub.client.broadcastMessage = function (message) {
-    }
-
-    /// Called when user has been joined to a game, contains state of game
-    gameHub.client.sendInitialState = function (gameInitialStateViewModel) {
-
-        // grab out the cards in hand
-
-        // to split cards in hand in two for view
-        var rowCounter = 1;
-        angular.forEach(gameInitialStateViewModel.DealtCards, function (item) {
-            $scope.cardsInHand.push(item);
-            if (rowCounter <= 5) {
-                $scope.cardsInTopRow.push(item);
-            } else {
-                $scope.cardsInBottomRow.push(item);
-            }
-            rowCounter++;
-        });
-
-        // grab out the players
-        angular.forEach(gameInitialStateViewModel.PlayerNames, function (item) {
-            $scope.playerNames.push(item);
-        });
-
-        $scope.blackCardText = gameInitialStateViewModel.BlackCardText;
-        $scope.playerGameGuid = gameInitialStateViewModel.PlayerInGameGuid;
-
-        $scope.$apply();
-    }
-
-    $.connection.hub.start().done(function () {
-        gameHub.server.joinServer("hank" + Math.floor(Math.random() * 100));
-    });
-
 });
 
 // directives
@@ -77,3 +27,4 @@ app.directive("sendsAnswerWhenClicked", function() {
         }
     }
 });
+
---
+++ Solutions/tomorrowcomestoday.web/Scripts/tomorrowcomestoday/app/controllers/gamecontroller.js
@@ -0,0 +1,49 @@
+﻿// define controllers
+angular.module("app").controller('GameController', function ($scope, $location, $anchorScroll) {
+    // Reference the auto-generated proxy for the hub.  
+    var gameHub = $.connection.gameHub;
+
+    $scope.cardsInHand = [];
+    $scope.cardsInTopRow = [];
+    $scope.cardsInBottomRow = [];
+
+    $scope.playerNames = [];
+    $scope.blackCardText = "";
+    $scope.players = [];
+    $scope.playerGameGuid = "";
+
+    gameHub.client.broadcastMessage = function (message) {
+    }
+
+    /// Called when user has been joined to a game, contains state of game
+    gameHub.client.sendInitialState = function (gameInitialStateViewModel) {
+
+        // grab out the cards in hand
+
+        // to split cards in hand in two for view
+        var rowCounter = 1;
+        angular.forEach(gameInitialStateViewModel.DealtCards, function (item) {
+            $scope.cardsInHand.push(item);
+            if (rowCounter <= 5) {
+                $scope.cardsInTopRow.push(item);
+            } else {
+                $scope.cardsInBottomRow.push(item);
+            }
+            rowCounter++;
+        });
+
+        // grab out the players
+        angular.forEach(gameInitialStateViewModel.PlayerNames, function (item) {
+            $scope.playerNames.push(item);
+        });
+
+        $scope.blackCardText = gameInitialStateViewModel.BlackCardText;
+        $scope.playerGameGuid = gameInitialStateViewModel.PlayerInGameGuid;
+
+        $scope.$apply();
+    }
+
+    $.connection.hub.start().done(function () {
+        gameHub.server.joinServer("hank" + Math.floor(Math.random() * 100));
+    });
+});
